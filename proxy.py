4d1087c02cdcefdf5c1cb16739d894adcc35dc5d
Swapnil Das <dasswapnil96@gmail.com>
2016-05-21 18:22:57 +0530

    Python Scipt to check Proxy Usage at IIT Delhi

--- proxy.py
+++ proxy.py
@@ -4,8 +4,8 @@ import time
 import requests
 from pyvirtualdisplay import Display
 
-display = Display(visible=0, size=(800, 600))
-display.start()
+#display = Display(visible=0, size=(800, 600))
+#display.start()
 browser = webdriver.Firefox()
 browser.get("https://proxy22.iitd.ernet.in/squish/") 
 time.sleep(10)
@@ -20,4 +20,4 @@ login_attempt.submit()
 proxy = browser.find_element_by_xpath('//center/table[2]/tbody/tr[3]/td[3]')
 print proxy.text
 browser.close()
-display.stop()
+#display.stop()
